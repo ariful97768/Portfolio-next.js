12c4de07a8a2d51a6edfadec626dec6ab1116792
ariful97768 <ariful97768@gmail.com>
2025-12-09 17:10:22 +0600

    added data them dark to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark">
       <body className={`${geistSans.variable} antialiased`}>{children}</body>
     </html>
   );
