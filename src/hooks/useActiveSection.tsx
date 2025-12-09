8db53077228c61e854932c8c20e4100f8591e015
ariful97768 <ariful97768@gmail.com>
2025-12-09 16:16:02 +0600

    Refactor FloatingNavbar to use custom hook for active section tracking

---
+++ src/hooks/useActiveSection.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { useEffect, useState } from "react";
+
+export default function useActiveSection() {
+  const [activeSection, setActiveSection] = useState("");
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll("section");
+      const scrollPosition = window.scrollY + window.innerHeight / 2;
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionBottom = sectionTop + section.offsetHeight;
+
+        if (scrollPosition >= sectionTop && scrollPosition < sectionBottom) {
+          setActiveSection(section.id);
+        }
+      });
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    handleScroll();
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return activeSection;
+}
